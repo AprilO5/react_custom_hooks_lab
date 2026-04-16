d3c43159a926c173b7d06b269c8159da541ade8a
April Lindie <april@MacBookPro.lan>
2026-04-15 20:45:15 -0400

    UselocalStorage custom hook for a form

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+  const [state, setState] = useState(() => {
+    return localStorage.getItem(key) || initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
 }
